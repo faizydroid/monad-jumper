e8e60e8e081e2bc74a3ee9ce37f712cc40405dbf
Faizan Ahmad <xfaizydroid@gmail.com>
2025-03-19 19:14:31 +0530

    fix: remove debug text and ensure React loads properly

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Make sure the root element exists in your HTML
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 ) 
